fd4c43ad98b2d13613ad9e6af6a6abba2b1b48c4
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-03-01 12:32:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import '../styles/App.css';
 const App = () => {
   const[size, setSize] = useState({
     height:320,
-    weight:320
+    width:320
   });
   // const[weight, setWeight] = useState(320);
   const handleChange = (e)=>{
@@ -13,8 +13,8 @@ const App = () => {
   return (
     <div id="main">
       <input type="range" name={"height"} min={100} max={800} value={size.height} id={"height-slider"} onChange={handleChange}/>
-      <input type="range" name='weight' min={100} max={800} value={size.weight} id={"weight-slider"} onChange={handleChange}/><br/>
-      <img src="src\image.jpeg" alt="image" id='resizable-img' height={size.height+"px"} width={size.weight+"px"}/>
+      <input type="range" name='width' min={100} max={800} value={size.width} id={"width-slider"} onChange={handleChange}/><br/>
+      <img src="src\image.jpeg" alt="image" id='resizable-img' height={size.height+"px"} width={size.width+"px"}/>
     </div>
   )
 }
